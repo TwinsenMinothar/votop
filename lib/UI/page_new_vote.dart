a5d742ecdf12af71db920d31e5557df27794b6b1
Pedro Igor <pedrode13souza@gmail.com>
2021-08-14 10:04:22 -0300

    Criando o Scaffold

--- lib/UI/page_new_vote.dart
+++ lib/UI/page_new_vote.dart
@@ -5,6 +5,11 @@ class NewVote extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Vo<T>op'),
+        centerTitle: true,
+      ),
+    );
   }
 }
