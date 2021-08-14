d041ded9eb7b98e6cbdc137a6c7c7f1130d1d7e8
Pedro Igor <pedrode13souza@gmail.com>
2021-08-14 17:09:53 -0300

    layout Welcome page

--- lib/UI/page_welcome.dart
+++ lib/UI/page_welcome.dart
@@ -1,44 +1,102 @@
 import 'package:flutter/material.dart';
 
-class WelcomePage extends StatelessWidget {
+class WelcomePage extends StatefulWidget {
   const WelcomePage({Key? key}) : super(key: key);
+
+  @override
+  _WelcomePageState createState() => _WelcomePageState();
+}
+
+class _WelcomePageState extends State<WelcomePage> {
+  bool obscureText = true;
+  IconData icon = Icons.visibility;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
         padding: EdgeInsets.symmetric(horizontal: 32),
-        child: Column(
+        child: Row(
           mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
+          //crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
-            Padding(
-              padding: const EdgeInsets.only(bottom: 32.0),
-              child: Text(
-                'Welcome to VoTop',
-                textAlign: TextAlign.center,
-                style: TextStyle(fontSize: 32),
+            Flexible(
+              flex: 1,
+              fit: FlexFit.tight,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                //crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(bottom: 32.0),
+                    child: Text(
+                      'Welcome to VoTop',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(fontSize: 32),
+                    ),
+                  ),
+                ],
               ),
             ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: ElevatedButton(
-                onPressed: () {},
-                child: Text('Sing Up'),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: ElevatedButton(
-                onPressed: () {},
-                child: Text('Login'),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: ElevatedButton(
-                onPressed: () {},
-                child: Text('New poll without Login'),
+            Flexible(
+              flex: 1,
+              fit: FlexFit.tight,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  TextFormField(
+                    decoration: InputDecoration(
+                      labelText: 'Email',
+                      icon: Icon(Icons.email),
+                    ),
+                  ),
+                  TextFormField(
+                    decoration: InputDecoration(
+                      labelText: 'Password',
+                      icon: Icon(Icons.lock),
+                      suffixIcon: IconButton(
+                        icon: Icon(icon),
+                        onPressed: () {
+                          setState(() {
+                            if (obscureText == true) {
+                              obscureText = false;
+                              icon = Icons.visibility_off;
+                            } else {
+                              obscureText = true;
+                              icon = Icons.visibility;
+                            }
+                          });
+                        },
+                      ),
+                    ),
+                    obscureText: obscureText,
+                  ),
+                  SizedBox(
+                    height: 16,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: ElevatedButton(
+                      onPressed: () {},
+                      child: Text('Login'),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: ElevatedButton(
+                      onPressed: () {},
+                      child: Text('Sing Up'),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: ElevatedButton(
+                      onPressed: () {},
+                      child: Text('New poll without Login'),
+                    ),
+                  ),
+                ],
               ),
             ),
           ],
