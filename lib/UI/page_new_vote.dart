8e0de2d74965c6942cbce1fde6715cc65db0cb59
Pedro Igor <pedrode13souza@gmail.com>
2021-08-14 10:01:45 -0300

    Criando a tela de criação dee novo voto

---
+++ lib/UI/page_new_vote.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class NewVote extends StatelessWidget {
+  const NewVote({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
