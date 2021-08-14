cc8c89685073d839b78acccd6aa75235df38a66f
Pedro Igor <pedrode13souza@gmail.com>
2021-08-14 10:27:44 -0300

    Criando o formulario de criação de caso

--- lib/UI/page_new_vote.dart
+++ lib/UI/page_new_vote.dart
@@ -10,6 +10,41 @@ class NewVote extends StatelessWidget {
         title: Text('Vo<T>op'),
         centerTitle: true,
       ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            Text(
+              'Qual é a ocasião?',
+              style: TextStyle(
+                fontSize: 32,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            TextFormField(
+              decoration: InputDecoration(
+                  labelText: 'Titulo', icon: Icon(Icons.subject)),
+            ),
+            TextFormField(
+              decoration: InputDecoration(
+                  labelText: 'Descrição', icon: Icon(Icons.description)),
+            ),
+
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(top: 16.0),
+                  child: ElevatedButton(
+                    onPressed: (){},
+                    child: Text('Continue'),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
