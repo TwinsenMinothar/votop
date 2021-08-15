d53e331a67b6342ced40fead171966f075bcfbbe
Camillo Rinaldi <camillo_rinaldi@yahoo.com.br>
2021-08-15 03:41:08 -0300

    wip: sending user to firebase

--- lib/UI/page_signup.dart
+++ lib/UI/page_signup.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:scoped_model/scoped_model.dart';
+import 'package:votop/models/user_model.dart';
 
 class SignUp extends StatefulWidget {
   const SignUp({Key? key}) : super(key: key);
@@ -8,63 +10,91 @@ class SignUp extends StatefulWidget {
 }
 
 class _SignUpState extends State<SignUp> {
-
   bool obscureText = true;
   IconData icon = Icons.visibility;
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-      body: Container(
-        padding: EdgeInsets.all(32),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            TextFormField(
-              decoration: InputDecoration(
-                labelText: 'Full Name',
-                icon: Icon(Icons.person),
-              ),
-            ),
-            TextFormField(
-              decoration: InputDecoration(
-                labelText: 'Email',
-                icon: Icon(Icons.email),
-              ),
-            ),
-            TextFormField(
-              decoration: InputDecoration(
-                labelText: 'Password',
-                icon: Icon(Icons.lock),
-                suffixIcon: IconButton(
-                  icon: Icon(icon),
-                  onPressed: () {
-                    setState(() {
-                      if (obscureText == true) {
-                        obscureText = false;
-                        icon = Icons.visibility_off;
-                      } else {
-                        obscureText = true;
-                        icon = Icons.visibility;
-                      }
-                    });
-                  },
-                ),
-              ),
-              obscureText: obscureText,
-            ),
-            Padding(
-              padding: const EdgeInsets.all(32.0),
-              child: ElevatedButton(
-                onPressed: () {},
-                child: Text('Cadastrar'),
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
+        appBar: AppBar(),
+        body: ScopedModelDescendant<UserModel>(
+          builder: (context, child, model) {
+            if (model.isLoading)
+              return Center(child: CircularProgressIndicator());
+            return Form(
+                key: _formKey,
+                child: Container(
+                  padding: EdgeInsets.all(32),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      TextFormField(
+                        decoration: InputDecoration(
+                          labelText: 'Full Name',
+                          icon: Icon(Icons.person),
+                        ),
+                      ),
+                      TextFormField(
+                        controller: _emailController,
+                        validator: (String? email) {
+                          if (email != null && email.isEmpty)
+                            return 'Email is required';
+                          return null;
+                        },
+                        decoration: InputDecoration(
+                          labelText: 'Email',
+                          icon: Icon(Icons.email),
+                        ),
+                      ),
+                      TextFormField(
+                        controller: _passwordController,
+                        validator: (String? email) {
+                          if (email != null && email.isEmpty)
+                            return 'Password is required';
+                          return null;
+                        },
+                        decoration: InputDecoration(
+                          labelText: 'Password',
+                          icon: Icon(Icons.lock),
+                          suffixIcon: IconButton(
+                            icon: Icon(icon),
+                            onPressed: () {
+                              setState(() {
+                                if (obscureText == true) {
+                                  obscureText = false;
+                                  icon = Icons.visibility_off;
+                                } else {
+                                  obscureText = true;
+                                  icon = Icons.visibility;
+                                }
+                              });
+                            },
+                          ),
+                        ),
+                        obscureText: obscureText,
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(32.0),
+                        child: ElevatedButton(
+                          onPressed: () async {
+                            if (_formKey.currentState!.validate()) {
+                              await model.signUp({
+                                'email': _emailController.text,
+                                'password': _passwordController.text
+                              });
+                            }
+                          },
+                          child: Text('Cadastrar'),
+                        ),
+                      ),
+                    ],
+                  ),
+                ));
+          },
+        ));
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:scoped_model/scoped_model.dart';
 import 'package:votop/UI/ui.dart';
+import 'package:votop/models/user_model.dart';
+import 'package:firedart/firedart.dart';
 
-void main() {
+const apiKey = 'AIzaSyA9xaSzba_OskjeVEXb0aQ5YWptteGNfrQ';
+const projectId = 'votop-tp';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  FirebaseAuth.initialize(apiKey, VolatileStore());
+  Firestore.initialize(projectId);
   runApp(MyApp());
 }
 
@@ -9,23 +18,24 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Vo<T>op',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: WelcomePage(),
-      debugShowCheckedModeBanner: false,
-    );
+    return ScopedModel<UserModel>(
+        model: UserModel(),
+        child: MaterialApp(
+          title: 'Vo<T>op',
+          theme: ThemeData(
+            // This is the theme of your application.
+            //
+            // Try running your application with "flutter run". You'll see the
+            // application has a blue toolbar. Then, without quitting the app, try
+            // changing the primarySwatch below to Colors.green and then invoke
+            // "hot reload" (press "r" in the console where you ran "flutter run",
+            // or simply save your changes to "hot reload" in a Flutter IDE).
+            // Notice that the counter didn't reset back to zero; the application
+            // is not restarted.
+            primarySwatch: Colors.blue,
+          ),
+          home: WelcomePage(),
+          debugShowCheckedModeBanner: false,
+        ));
   }
 }
-
