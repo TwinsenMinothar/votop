5f8de88d8ebf76caa724066b4273fe7318b99d51
gustavo.presoti <gu.presoti@gmail.com>
2021-08-23 21:00:12 -0300

    fix: card buttons and layout

--- lib/UI/page_voter_panel.dart
+++ lib/UI/page_voter_panel.dart
@@ -15,13 +15,7 @@ class VoterPanel extends StatelessWidget {
       appBar: AppBar(
         title: Text('VoTop | Painel'),
         centerTitle: true,
-        leading: InkWell(
-          onTap: () {
-            Navigator.of(context)
-                .push(MaterialPageRoute(builder: (context) => EditProfile()));
-          },
-          child: Icon(Icons.manage_accounts),
-        ),
+        automaticallyImplyLeading: false,
         actions: [
           Padding(
             padding: EdgeInsets.only(right: 50),
@@ -50,51 +44,55 @@ class VoterPanel extends StatelessWidget {
           ),
         ],
       ),
-      body: Container(
-        height: double.infinity,
-        padding: EdgeInsets.only(top: 20),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: [
-            Text(
-              'Bem vindo ao VoTop, Fulano de Tal',
-              style: TextStyle(
-                fontSize: 48,
-                fontWeight: FontWeight.w600,
-              ),
-            ),
-            Padding(
-              padding: EdgeInsets.symmetric(horizontal: 150),
-              child: Divider(height: 25),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
+      body: ScopedModelDescendant<UserModel>(
+        builder: (context, child, model) {
+          return Container(
+            height: double.infinity,
+            padding: EdgeInsets.only(top: 20),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
               children: [
-                CardButton(
-                  title: 'Depositar\nVoto',
-                  color: _primaryColor,
-                  icon: Icons.note_alt_rounded,
-                  nextPage: () => VotingList(
-                    title: 'Escolha uma votação para participar',
-                    onChoosing: (poll) {
-                      Navigator.of(context).push(
-                        MaterialPageRoute(
-                          builder: (context) => Voting(poll: poll),
-                        ),
-                      );
-                    },
+                Text(
+                  'Bem vindo ao VoTop, ${model.userData['name']}',
+                  style: TextStyle(
+                    fontSize: 48,
+                    fontWeight: FontWeight.w600,
                   ),
                 ),
-                CardButton(
-                  title: 'Criar\nVotação',
-                  color: _primaryColor,
-                  icon: Icons.add_task_rounded,
-                  nextPage: () => NewVote(),
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 150),
+                  child: Divider(height: 25),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    CardButton(
+                      title: 'Depositar\nVoto',
+                      color: _primaryColor,
+                      icon: Icons.note_alt_rounded,
+                      nextPage: () => VotingList(
+                        title: 'Escolha uma votação para participar',
+                        onChoosing: (poll) {
+                          Navigator.of(context).push(
+                            MaterialPageRoute(
+                              builder: (context) => Voting(poll: poll),
+                            ),
+                          );
+                        },
+                      ),
+                    ),
+                    CardButton(
+                      nextPage: () => EditProfile(),
+                      icon: Icons.manage_accounts_rounded,
+                      color: _primaryColor,
+                      title: 'Editar Perfil',
+                    ),
+                  ],
                 ),
               ],
             ),
-          ],
-        ),
+          );
+        },
       ),
     );
   }
